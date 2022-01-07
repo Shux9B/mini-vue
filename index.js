1c040b260c81743e336897ac3089945c4ad28db0
fewang <18511694913@163.com>
2022-01-07 22:50:34 +0800

    feat(observer)增加array的拦截

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import Vue from "./Vue.js"
 let instance = new Vue({
     data: {
-        a: 1
+        a: 1,
+        b: [1,0,4]
     },
     render () {
         console.log('触发并打印了：', this.a)
     }
 })
 
-instance.a = 2
-instance.a = 3
-instance.a = 4
+// instance.a = 2
+// instance.a = 3
+// instance.a = 4
+console.log(instance.b)
+instance.b.push(9)
+console.log(instance.b)
--- libs/core/Observer.js
+++ libs/core/Observer.js
@@ -1,26 +1,60 @@
 import Dep from "./Dep.js"
+
+const ArrayFunc = ['push', 'shift', 'pop', 'unshift', 'reverse', 'sort', 'splice']
 export default class {
+    #dep = new Dep()
     constructor(target) {
-        
-        Object.defineProperty(target, '_dep', {
-            enumerable: false,
-            value: new Dep()
-        })
         for (let k in target) {
             let initVal = target[k]
-            Object.defineProperty(target, k, {
-                get() {
-                    target._dep.depend()
-                    return initVal
-                },
-                set(val) {
-                    if (initVal !== val) {
-                        initVal = val
-                        target._dep.notify()
-                    }
-                }
-            })
+            if (Array.isArray(initVal)) {
+                this.#reactiveArray(k, initVal)
+            } else {
+                this.#reactiveCommon(k, initVal)
+            }
+            
         }
-        return target
+    }
+    #reactiveCommon (key, initVal) {
+        const vm = this
+        Object.defineProperty(vm, key, {
+            get() {
+                vm.#dep.depend()
+                return initVal
+            },
+            set(val) {
+                if (initVal !== val) {
+                    initVal = val
+                    vm.#dep.notify()
+                }
+            }
+        })
+    }
+    
+    #reactiveArray (key, initVal) {
+        const vm = this
+        let obj = Object.create(Array.prototype)
+        ArrayFunc.forEach(funName => {
+            obj[funName] = function ()  {
+                Array.prototype[funName].call(this, ...arguments)
+                console.log(`拦截方法${funName}成功了`)
+                vm.#dep.notify()
+            }
+        })
+        let t = Object.create(obj)
+        initVal.forEach(val => t.push(val))
+        initVal = t
+        // initVal = 
+        Object.defineProperty(vm, key, {
+            get() {
+                vm.#dep.depend()
+                return initVal
+            },
+            set(val) {
+                if (initVal !== val) {
+                    initVal = val
+                    vm.#dep.notify()
+                }
+            }
+        })
     }
 }
